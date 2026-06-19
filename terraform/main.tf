54e5b068c0acd5df23d801313b76dc74ec833bbf
EC2 Default User <ec2-user@ip-172-31-24-94.ap-southeast-2.compute.internal>
2026-06-19 17:31:43 +0000

    Added gitignore & terraform networking

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,96 @@
-resource "aws_vpc" "devops_vpc" {
-cidr_block = "10.0.0.0/16"
+resource "aws_internet_gateway" "devops_igw" {
 
-tags = {
-  Name="devops-vpc"
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-igw"
+  }
 }
 
+resource "aws_subnet" "public_subnet" {
+
+  vpc_id = aws_vpc.devops_vpc.id
+
+  cidr_block = "10.0.1.0/24"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
+
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.devops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devops_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  route_table_id = aws_route_table.public_rt.id
+
+}
+
+resource "aws_security_group" "devops_sg" {
+
+  name = "devops-security-group"
+
+  vpc_id = aws_vpc.devops_vpc.id
+
+  ingress {
+
+    description = "SSH"
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+
+    description = "HTTP"
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "devops-sg"
+
+  }
+}
+
